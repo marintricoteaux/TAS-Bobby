ee3c529382c5b45f7111d74d11353751374de1b4
Marin Tricoteaux <marintcx@icloud.com>
2024-08-28 16:04:11 +0200

    changement pour la vidéo

--- scripts/tas.js
+++ scripts/tas.js
@@ -1,15 +1,15 @@
 /*
 Dans ce fichier j'utiliserai les fonctions natives de js :
 setTimeout(() => {}, tempsEnMillisecondes); Qui permet d'executer des actions après un temps (en ms) donné
-setIntervall(() => {}, tempsEnMillisecondes); Qui permet d'execurer des actions à une intervalles (en ms) donné 
+setInterval(() => {}, tempsEnMillisecondes); Qui permet d'execurer des actions à une intervalles (en ms) donné 
 */
 
-setTimeout(() => {
+setInterval(() => {
     TASJump = true
-    console.log("TASJUMP = TRUE après 10s")
-}, 10000)
+    console.log("5sec : TASJUMP = TRUE")
+}, 5000)
 
-setTimeout(() => {
+setInterval(() => {
     TASJump = false
-    console.log("TASJUMP = FALSE après 15s")
-}, 15000)
+    console.log("5.1sec : TASJUMP = FALSE")
+}, 5100)
